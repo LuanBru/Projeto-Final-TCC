a279ad3b979ed14c009da4fa29272a26110a0301
LuanBru <luan.brunoms@gmail.com>
2023-05-31 19:56:16 -0300

    Database Feito

---
+++ Projeto Final TCC/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Projeto_Final_TCC.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Projeto_Final_TCC.Models.Fotos.DTB1>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Projeto_Final_TCC.Models.Fotos.DTB1 context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- Projeto Final TCC/Models/Fotos.cs
+++ Projeto Final TCC/Models/Fotos.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Projeto_Final_TCC.Models
 {
     public class Fotos
     {
-        public int ID_Foto { get; set; }
+        [Key]
+        public int FotoId { get; set; }
         public int ID_Usuario { get; set; }
         public int ID_Animal { get; set; }
         public string GPS { get; set; }
         public int ID_Verificado { get; set; }
         public string OBs { get; set; }
 
+        public class DTB1 : DbContext
+        {
+            public DbSet<Fotos> Foto { get; set; }
+        }
     }
 }
