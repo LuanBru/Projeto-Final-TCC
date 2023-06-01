511bb4e26d07e24d45d3c2b93c7fa90cc80239c9
LuanBru <luan.brunoms@gmail.com>
2023-06-01 20:18:06 -0300

    Longitude de Latitude

--- Projeto Final TCC/Models/Fotos.cs
+++ Projeto Final TCC/Models/Fotos.cs
@@ -13,13 +13,17 @@ namespace Projeto_Final_TCC.Models
         public int FotoId { get; set; }
         public int ID_Usuario { get; set; }
         public int ID_Animal { get; set; }
-        public string GPS { get; set; }
+        public string longitudo { get; set; }
+        public string latitude { get; set; }
         public int ID_Verificado { get; set; }
         public string OBs { get; set; }
 
         public class DTB1 : DbContext
         {
             public DbSet<Fotos> Foto { get; set; }
+            public DbSet<Usuario> usuario { get; set; }
+            public DbSet<Bio> biologo { get; set; }
+            public DbSet<ADM> administrador { get; set; }
         }
     }
 }
--- Projeto Final TCC/Models/Usuario.cs
+++ Projeto Final TCC/Models/Usuario.cs
@@ -2,17 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Projeto_Final_TCC.Models
 {
     public class Usuario
     {
-        public int ID_Usuario { get; set; }
+        [Key]
+        public int Id_usuario { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
-        public string ID_Bio { get; set; }
 
-        public bool V_Biologo { get; set; }
+
     }
+
+    public class Bio
+    {
+        [Key]
+        public int Id_bio { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+
+    }
+
+    public class ADM
+    {
+        [Key]
+        public int Id_adm { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+
+    }
+
 }
