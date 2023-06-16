9ebe57330e7bd39876c863b7ed63904ff8d7dca5
LuanBru <luan.brunoms@gmail.com>
2023-06-15 23:25:25 -0300

    Imagem Firebase

--- Projeto Final TCC/Controllers/ImagemController.cs
+++ Projeto Final TCC/Controllers/ImagemController.cs
@@ -1,36 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Projeto_Final_TCC.Models;
+﻿using Firebase.Storage;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace Projeto_Final_TCC.Controllers
 {
     public class ImagemController : Controller
     {
-        [HttpPost]
-        public Microsoft.AspNetCore.Mvc.IActionResult Create(Fotos model)
+        private readonly IConfiguration _configuration;
+
+        public ImagemController()
         {
-            if (ModelState.IsValid)
+            // Construtor sem parâmetros
+        }
+        public ImagemController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Upload(HttpPostedFileBase file)
+        {
+            // Verifica se o arquivo foi enviado corretamente
+            if (file != null && file.ContentLength > 0)
             {
-                if (model.Imagem != null && model.Imagem.Length > 0)
+                try
                 {
-                    var foto = new Fotos
-                    {
-                        ID_Usuario = model.ID_Usuario,
-                        ID_Animal = model.ID_Animal,
-                        Longitude = model.Longitude,
-                        Latitude = model.Latitude,
-                        ID_Verificado = model.ID_Verificado,
-                        Imagem = model.Imagem,
-                        OBs = model.OBs
-                    };
+                    // Obter as configurações do Firebase do arquivo Web.config
+                    var FirebaseConfig = _configuration.GetSection("FirebaseConfig").Value;
 
-                    // Salvar o objeto 'foto' no banco de dados usando o Entity Framework Core
+                    // Cria uma instância do FirebaseStorage com as configurações
+                    var task = new FirebaseStorage(FirebaseConfig)
+                        .Child("imagens") // Especifica o caminho para o armazenamento das imagens
+                        .Child(Guid.NewGuid().ToString()) // Define um nome único para a imagem
+                        .PutAsync(file.InputStream);
 
-                    return RedirectToAction("Principal") as Microsoft.AspNetCore.Mvc.IActionResult;
+                    // Aguarda o término do upload
+                    var imageUrl = await task;
+
+                    // Retorna a URL da imagem no Firebase Storage
+                    return Content(imageUrl);
+                }
+                catch (Exception ex)
+                {
+                    // Tratar possíveis erros
+                    return Content("Erro ao enviar a imagem: " + ex.Message);
                 }
             }
 
-            return (IActionResult)View(model);
+            // Retorna uma mensagem de erro caso não tenha recebido nenhum arquivo
+            return Content("Nenhum arquivo enviado.");
         }
     }
 }
